6e0fb4b5bf8eaf7e7286a2d92e83f50716355b2e
Nhựt Nguyễn <thangcao1906@gmail.com>
2022-10-25 09:16:52 +0700

    25/10/2022

---
+++ Project_BE_Solution/Project_BE_Solution/Models/Data/EF/BEDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace Project_BE_Solution.Models.Data.EF
+{
+    public class BEDbContextFactory : IDesignTimeDbContextFactory<BEDbContext>
+    {
+        public BEDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("BeSolutionDb");
+
+            var optionsBuilder = new DbContextOptionsBuilder<BEDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new BEDbContext(optionsBuilder.Options);
+        }
+    }
+}
